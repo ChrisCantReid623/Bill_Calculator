2098dd1f6ecdd2b35d74438b822d26a5d006df6e
Christopher Reid <chrisreid623@gmail.com>
2023-01-29 17:56:13 -0700

    Update bills.py

--- bills.py
+++ bills.py
@@ -15,13 +15,41 @@ CURRENT = {'rent': 1338.10,
            'internet': 105.12,
            'cell': 80.00
            }
+def main(current):
+    display_title(current)
+    command = input('Enter a command: ')
+    print()
+    if command.upper() == 'SET':
+        bills_preset(current)
+    elif command.upper() == 'CUSTOM':
+        bills_custom()
 
+    again = input('Would you like to run this again? (Y/N): ')
+    if again.upper() == 'Y':
+        main()
+    elif again.upper() == 'N':
+        exit()
+def display_title(current_bills):
+    ascii_banner = pyfiglet.figlet_format("Bill Calculator")
+    print(ascii_banner)
+    print('CURRENT BILLS:')
+    for name, amount in current_bills.items():
+        print(f'{name}: ${amount}')
+    print()
+    print(' ____________________')
+    print(' \ CHOOSE A COMMAND  \\')
+    print('  \___________________\\')
+    print()
+    print('SET: Default preset bills (Current as of January 2023).')
+    print('CUSTOM: Customize bill input.')
+    print()
 def bills_preset(curr_bills):
     """Bill presets current as of October 2022."""
     curr_bills['electricity'] = get_electricity_bill()
     Bills_PRESET(curr_bills)
-
-
+def get_electricity_bill():
+    """Prompts user for electric bill due to variation."""
+    return float(input('How much was the electric bill this month?: '))
 def bills_custom():
     """Returns a dictionary of bills as determined by user input."""
 
@@ -37,24 +65,6 @@ def bills_custom():
         bills[bill_name] = float(amount)
     print()
     Bills_CUSTOM(bills)
-
-
-def display_title(current_bills):
-    ascii_banner = pyfiglet.figlet_format("Bill Calculator")
-    print(ascii_banner)
-    print('CURRENT BILLS:')
-    for name, amount in current_bills.items():
-        print(f'{name}: ${amount}')
-    print()
-    print(' ____________________')
-    print(' \ CHOOSE A COMMAND  \\')
-    print('  \___________________\\')
-    print()
-    print('SET: Default preset bills (Current as of January 2023).')
-    print('CUSTOM: Customize bill input.')
-    print()
-
-
 def split_bills(total):
     """Prints the amount based on the number of roommates. """
     roommates = float(input('How many roommates are splitting the bill?: '))
@@ -65,20 +75,11 @@ def split_bills(total):
     else:
         split = round((total / roommates), 2)
         return split
-
-
 def resources():
     print('Here\'s a little book I wrote on personal finance. Hope you enjoy and learn! :)')
     phrase = 'Open this URL -> '
     print(phrase + 'https://drive.google.com/file/d/1BkpjseROzhfaKMPNRIojX3Mkw2d3PkZU/view?usp=share_link')
     print()
-
-
-def get_electricity_bill():
-    """Prompts user for electric bill due to variation."""
-    return float(input('How much was the electric bill this month?: '))
-
-
 class Bills_PRESET:
     def __init__(self, bills):
         """Passed preset values for bills as arguments."""
@@ -122,8 +123,6 @@ class Bills_PRESET:
         # Resources
         print('RESOURCES:')
         resources()
-
-
 class Bills_CUSTOM:
     def __init__(self, bills):
         """Passed a dictionary containing the bills as an argument."""
@@ -167,22 +166,4 @@ class Bills_CUSTOM:
         # Resources
         print('RESOURCES:')
         resources()
-
-
-def main(current):
-    display_title(current)
-    command = input('Enter a command: ')
-    print()
-    if command.upper() == 'SET':
-        bills_preset(current)
-    elif command.upper() == 'CUSTOM':
-        bills_custom()
-
-    again = input('Would you like to run this again? (Y/N): ')
-    if again.upper() == 'Y':
-        main()
-    elif again.upper() == 'N':
-        exit()
-
-
 main(CURRENT)
